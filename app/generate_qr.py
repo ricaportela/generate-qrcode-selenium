68788c895c072437af59156af041470ad38431e2
Ricardo Portela da Silva <ricardo@pop-os.localdomain>
2018-12-09 22:31:33 -0200

    Initial Setup

--- app/generate_qr.py
+++ app/generate_qr.py
@@ -22,7 +22,7 @@ class GenerateQR():
         "url": "https://qr.ioi.tw/pt/",
     }
 
-    def __init__(self, headless=True, browser='Chrome'):  # em producao TRUE
+    def __init__(self, headless=False, browser='Chrome'):  # change to true in production
 
         # self.driver = webdriver.PhantomJS(executable_path="../bin/phantomjs")
 
@@ -41,43 +41,63 @@ class GenerateQR():
                     executable_path="bin/geckodriver", firefox_options=options)
 
     def main(self):
+        print("Opening qrcode page")
+        self.driver.set_page_load_timeout(930)
+        try:
+            self.driver.get(GenerateQR.URLs["url"])
+        except TimeoutException:
+            print("Timeout!!!")
+            return
+        except ValueError:
+            return
         time.sleep(5)  # Let the user actually see something!
-        raw = self.driver.find_element_by_xpath(
-            '/html/body/div[2]/div/div[1]/ul/li[8]/a')
+        raw_xpath = '/html/body/div[2]/div/div[1]/ul/li[8]/a'
+        raw = self.driver.find_element_by_xpath(raw_xpath)
         self.driver.execute_script("arguments[0].click()", raw)
+
         txtarea = self.driver.find_element_by_id('text')
         txtarea.clear()
         txtarea.send_keys('parametro de geração')
         time.sleep(5)  # Let the user actually see something!
 
-        btn_tamanho_cor = self.driver.find_element_by_xpath('//*[@id="s1"]')
+        btn_tamanho_cor_xpath = '//*[@id="s1"]'
+        btn_tamanho_cor = self.driver.find_element_by_xpath(
+            btn_tamanho_cor_xpath)
         self.driver.execute_script("arguments[0].click()", btn_tamanho_cor)
+
         tamanho_imagem = self.driver.find_element_by_id('qrsi')
         self.driver.execute_script(
             "arguments[0].setAttribute('value','200')", tamanho_imagem)
-        tamanho_fronteira = self.driver.find_element_by_xpath(
-            '//*[@id="borderr"]/label[2]')
-        self.driver.execute_script("arguments[0].click()", tamanho_fronteira)
-        correcao_erros = self.driver.find_element_by_xpath(
-            '//*[@id="errr"]/label[4]')
-        self.driver.execute_script("arguments[0].click()", correcao_erros)
-        element = self.driver.find_element_by_xpath(
-            '//*[@id="radiusr"]/label[2]')
-        self.driver.execute_script("arguments[0].click()", element)
+
+        size_frontier_xpath = '//*[@id="borderr"]/label[2]'
+        size_frontier = self.driver.find_element_by_xpath(size_frontier_xpath)
+        self.driver.execute_script("arguments[0].click()", size_frontier)
+
+        error_cor_xpath = '//*[@id="errr"]/label[4]'
+        error_cor = self.driver.find_element_by_xpath(error_cor_xpath)
+        self.driver.execute_script("arguments[0].click()", error_cor)
+
+        radius_xpath = '//*[@id="radiusr"]/label[2]'
+        radius = self.driver.find_element_by_xpath(radius_xpath)
+        self.driver.execute_script("arguments[0].click()", radius)
 
         time.sleep(5)  # Let the user actually see something!
 
         btn_mais_logotipo = self.driver.find_element_by_xpath('//*[@id="s2"]')
         self.driver.execute_script("arguments[0].click()", btn_mais_logotipo)
-        fotos = self.driver.find_element_by_xpath(
-            '//*[@id="labelmode"]/label[3]')
-        self.driver.execute_script("arguments[0].click()", fotos)
-        seleciona_foto = self.driver.find_element_by_id('image')
-        seleciona_foto.send_keys(
-            '/home/ricardo/dev/renivandro/tabelionato_xisto.jpeg')
-        dimensao = self.driver.find_element_by_xpath(
-            '//*[@id="fontsize"]/label[4]')
-        self.driver.execute_script("arguments[0].click()", dimensao)
+
+        photo_xpath = '//*[@id="labelmode"]/label[3]'
+        photo = self.driver.find_element_by_xpath(photo_xpath)
+        self.driver.execute_script("arguments[0].click()", photo)
+
+        sel_photo = self.driver.find_element_by_id('image')
+        sel_photo_path = '/home/ricardo/dev/renivandro/tabelionato_xisto.jpeg'
+        sel_photo.send_keys(sel_photo_path)
+
+        dimension_xpath = '//*[@id="fontsize"]/label[4]'
+        dimension = self.driver.find_element_by_xpath(dimension_xpath)
+        self.driver.execute_script("arguments[0].click()", dimension)
+
         time.sleep(5)  # Let the user actually see something!
 
         # <input type="file" class="custom-file-input" id="image">
@@ -87,3 +107,6 @@ class GenerateQR():
         baixa_qrcode = self.driver.find_element_by_id('dlbtn')
         self.driver.execute_script(param, baixa_qrcode)
         baixa_qrcode.click()
+
+    def close(self):
+        self.driver.quit()
--- core.py
+++ core.py
@@ -6,3 +6,4 @@ from app.generate_qr import GenerateQR
 
 if __name__ == "__main__":
     qr = GenerateQR()
+    qr.main()
