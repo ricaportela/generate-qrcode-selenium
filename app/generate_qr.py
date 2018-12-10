0633c3eb65737841c79e483075b8b5c498df95e7
ricaportela <ricaportela@gmail.com>
2018-12-10 07:34:38 -0300

    Update script download image

--- app/generate_qr.py
+++ app/generate_qr.py
@@ -30,8 +30,13 @@ class GenerateQR():
             options = Options()
             if headless:
                 options.add_argument('--headless')
+                # options.add_argument("--start-maximized")
+                # options.add_argument("download.default_directory=C:/xisto")
+                options.add_argument('--ignore-certificate-errors')
+                options.add_argument("--test-type")
+
             self.driver = webdriver.Chrome(
-                executable_path="bin/chromedriver", chrome_options=options)
+                executable_path="bin/chromedriver.exe", chrome_options=options)
         else:
             if browser == 'Firefox':
                 options = FireFoxoptions()
@@ -50,7 +55,7 @@ class GenerateQR():
             return
         except ValueError:
             return
-        time.sleep(5)  # Let the user actually see something!
+        time.sleep(2)  # Let the user actually see something!
         raw_xpath = '/html/body/div[2]/div/div[1]/ul/li[8]/a'
         raw = self.driver.find_element_by_xpath(raw_xpath)
         self.driver.execute_script("arguments[0].click()", raw)
@@ -58,7 +63,7 @@ class GenerateQR():
         txtarea = self.driver.find_element_by_id('text')
         txtarea.clear()
         txtarea.send_keys('parametro de geração')
-        time.sleep(5)  # Let the user actually see something!
+        time.sleep(2)  # Let the user actually see something!
 
         btn_tamanho_cor_xpath = '//*[@id="s1"]'
         btn_tamanho_cor = self.driver.find_element_by_xpath(
@@ -81,7 +86,7 @@ class GenerateQR():
         radius = self.driver.find_element_by_xpath(radius_xpath)
         self.driver.execute_script("arguments[0].click()", radius)
 
-        time.sleep(5)  # Let the user actually see something!
+        time.sleep(2)  # Let the user actually see something!
 
         btn_mais_logotipo = self.driver.find_element_by_xpath('//*[@id="s2"]')
         self.driver.execute_script("arguments[0].click()", btn_mais_logotipo)
@@ -91,22 +96,31 @@ class GenerateQR():
         self.driver.execute_script("arguments[0].click()", photo)
 
         sel_photo = self.driver.find_element_by_id('image')
-        sel_photo_path = '/home/ricardo/dev/renivandro/tabelionato_xisto.jpeg'
+        sel_photo_path = 'c:/xisto/tabelionato_xisto.jpeg'
         sel_photo.send_keys(sel_photo_path)
 
         dimension_xpath = '//*[@id="fontsize"]/label[4]'
         dimension = self.driver.find_element_by_xpath(dimension_xpath)
         self.driver.execute_script("arguments[0].click()", dimension)
 
-        time.sleep(5)  # Let the user actually see something!
+        time.sleep(2)  # Let the user actually see something!
 
         # <input type="file" class="custom-file-input" id="image">
         # <img id="img-buffer" src="../dummy.png" style="display: none;">
+        time.sleep(5)  # Let the user actually see something!
+
+        image_src =  self.driver.find_element_by_id('img-buffer')
+        image_src = "arguments[0].setAttribute('src','" + "images/teste2.png" + "')"
+
 
         param = "arguments[0].setAttribute('download','" + "teste2.png" + "')"
         baixa_qrcode = self.driver.find_element_by_id('dlbtn')
         self.driver.execute_script(param, baixa_qrcode)
         baixa_qrcode.click()
+
+        time.sleep(5)  # Let the user actually see something!
+
+        self.close()
 
     def close(self):
         self.driver.quit()
