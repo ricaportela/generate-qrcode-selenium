dd0e727462b682bb94b3c8cf78efd704a5e02ca7
ricaportela <ricaportela@gmail.com>
2018-12-11 04:37:34 -0300

    Change calling xpath buttons

--- app/main.py
+++ app/main.py
@@ -12,11 +12,9 @@ import time
 from datauri import DataURI
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FireFoxoptions
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
@@ -26,7 +24,7 @@ class GenerateQR():
         "url": "https://qr.ioi.tw/pt/",
     }
 
-    def __init__(self, headless=False, browser='Chrome'):  # change to true in production
+    def __init__(self, headless=True, browser='Chrome'):  # change to true in production
 
         # self.driver = webdriver.PhantomJS(executable_path="../bin/phantomjs")
 
@@ -35,12 +33,22 @@ class GenerateQR():
             options.add_argument('--no-sandbox')
             options.add_argument('--window-size=1420,1080')
             options.add_argument('--disable-gpu')
+            options.add_argument('--disable-setuid-sandbox')
+            options.add_argument("--proxy-server='direct://'")
+            options.add_argument("--proxy-bypass-list=*")
+            options.add_argument("--disable-software-rasterizer")
+            options.add_argument("--mute-audio")
+            options.add_argument("--hide-scrollbars")
+            options.add_argument("--remote-debugging-port=9222")
+            options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")
+            # options.add_argument("--log-path=chromedriver.log")
+            # options.add_argument("--verbose")
+
             if headless:
                 options.add_argument('--headless')
 
             self.driver = webdriver.Chrome(
-                executable_path="bin/chromedriver.exe", options=options, 
-                service_args=['--verbose', '--log-path=chromedriver.log'])
+                executable_path="bin/chromedriver.exe", options=options)
         else:
             if browser == 'Firefox':
                 options = FireFoxoptions()
@@ -60,39 +68,30 @@ class GenerateQR():
             return
 
     def parameters(self, numero):
-        time.sleep(2)  # Let the user actually see something!
-        self.newmethod555('/html/body/div[2]/div/div[1]/ul/li[8]/a')
-
+        self.driver.implicitly_wait(3)        
+        self.findbyxpath('/html/body/div[2]/div/div[1]/ul/li[8]/a') # RAW button
+        self.findbyxpath('//*[@id="s1"]') # definir tamanho e cor button
+                                  
         txtarea = self.driver.find_element_by_id('text')
         txtarea.clear()
-        txtarea.send_keys('parametro de geração')
-        # time.sleep(2)  # Let the user actually see something!
-
-        btn_tamanho_cor_xpath = '//*[@id="s1"]'
-        btn_tamanho_cor = self.driver.find_element_by_xpath(
-            btn_tamanho_cor_xpath)
-        self.driver.execute_script("arguments[0].click()", btn_tamanho_cor)
+        txtarea.send_keys(numero)
 
         tamanho_imagem = self.driver.find_element_by_id('qrsi')
         self.driver.execute_script(
             "arguments[0].setAttribute('value','200')", tamanho_imagem)
 
-        size_frontier_xpath = '//*[@id="borderr"]/label[2]'
-        size_frontier = self.driver.find_element_by_xpath(size_frontier_xpath)
-        self.driver.execute_script("arguments[0].click()", size_frontier)
+        self.findbyxpath('//*[@id="borderr"]/label[2]') # tamanho da fronteira
+        self.findbyxpath('//*[@id="errr"]/label[4]') # correcao de cor
 
-        error_cor_xpath = '//*[@id="errr"]/label[4]'
-        error_cor = self.driver.find_element_by_xpath(error_cor_xpath)
-        self.driver.execute_script("arguments[0].click()", error_cor)
-
+        self.findbyxpath('//*[@id="s1"]') # definir tamanho e cor button
         radius_xpath = '//*[@id="radiusr"]/label[2]'
         radius = self.driver.find_element_by_xpath(radius_xpath)
         self.driver.execute_script("arguments[0].click()", radius)
 
-        # time.sleep(2)  # Let the user actually see something!
-
+        self.findbyxpath('//*[@id="s1"]') # definir tamanho e cor button
         btn_mais_logotipo = self.driver.find_element_by_xpath('//*[@id="s2"]')
         self.driver.execute_script("arguments[0].click()", btn_mais_logotipo)
+        self.findbyxpath('//*[@id="s1"]') # definir tamanho e cor button
 
         photo_xpath = '//*[@id="labelmode"]/label[3]'
         photo = self.driver.find_element_by_xpath(photo_xpath)
@@ -106,36 +105,29 @@ class GenerateQR():
         dimension = self.driver.find_element_by_xpath(dimension_xpath)
         self.driver.execute_script("arguments[0].click()", dimension)
 
-        # time.sleep(2)  # Let the user actually see something!
-
-        # <input type="file" class="custom-file-input" id="image">
-        # <img id="img-buffer" src="../dummy.png" style="display: none;">
-        # time.sleep(5)  # Let the user actually see something!
-
-        # image_src = self.driver.find_element_by_id('img-buffer')
-        # image_src = "arguments[0].setAttribute('src','" + \
-        #     "images/teste2.png" + "')"
-
         imagem = str(numero.strip() + '.png')
         print(imagem)
         param = "arguments[0].setAttribute('download','" + imagem + "')"
         baixa_qrcode = self.driver.find_element_by_id('dlbtn')
         self.driver.execute_script(param, baixa_qrcode)
 
-        time.sleep(5)  # Let the user actually see something!
-
         baixa_qrcode.click()
+        self.driver.implicitly_wait(5)        
 
         url = baixa_qrcode.get_property('href')
         uri = DataURI(url)
         with open("c:/xisto/" + imagem, 'wb') as f:
             f.write(uri.data)
 
-    def newmethod555(self, param_xpath):
-        element = self.driver.find_element_by_xpath(param_xpath)
-        self.driver.execute_script("arguments[0].click()", element)
+    def findbyxpath(self, param_xpath):
+        #element = self.driver.find_element_by_xpath(param_xpath)
+        #self.driver.execute_script("arguments[0].click()", element)
+        print("Passou por aqui", param_xpath)
+        try:
+            element = WebDriverWait(self.driver, 1).until(EC.presence_of_element_located((By.XPATH, param_xpath)))
 
-        # time.sleep(5)  # Let the user actually see something!
+        finally:
+            self.driver.execute_script("arguments[0].click()", element)
 
     def close(self):
         self.driver.quit()
