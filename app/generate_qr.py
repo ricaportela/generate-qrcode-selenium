5ca84d1d81f9dd1411aace3c64dbc0ea7cbd52db
ricaportela <ricaportela@gmail.com>
2018-12-11 06:01:16 -0300

    Update xpath functions

--- app/generate_qr.py
+++ app/generate_qr.py
@@ -4,29 +4,26 @@
 # dimensão: 4
 # central
 # central
-""" AutomGenerate Qrcode form website."""
+""" Automatic Generate Qrcode from website."""
 import logging
 import os
 import sys
-import time
+from time import sleep, time
 from datauri import DataURI
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FireFoxoptions
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-
 
 class GenerateQR():
     URLs = {
         "url": "https://qr.ioi.tw/pt/",
     }
 
-    def __init__(self, headless=False, browser='Chrome'):  # change to true in production
+    def __init__(self, headless=True, browser='Chrome'):  # change to true in production
 
         # self.driver = webdriver.PhantomJS(executable_path="../bin/phantomjs")
 
@@ -35,12 +32,22 @@ class GenerateQR():
             options.add_argument('--no-sandbox')
             options.add_argument('--window-size=1420,1080')
             options.add_argument('--disable-gpu')
+            options.add_argument('--disable-setuid-sandbox')
+            options.add_argument("--proxy-server='direct://'")
+            options.add_argument("--proxy-bypass-list=*")
+            options.add_argument("--disable-software-rasterizer")
+            options.add_argument("--mute-audio")
+            options.add_argument("--hide-scrollbars")
+            options.add_argument("--remote-debugging-port=9222")
+            options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")
+            # options.add_argument("--log-path=chromedriver.log")
+            # options.add_argument("--verbose")
+
             if headless:
                 options.add_argument('--headless')
 
             self.driver = webdriver.Chrome(
-                executable_path="bin/chromedriver.exe", chrome_options=options, 
-                service_args=['--verbose', '--log-path=chromedriver.log'])
+                executable_path="bin/chromedriver.exe", options=options)
         else:
             if browser == 'Firefox':
                 options = FireFoxoptions()
@@ -49,8 +56,7 @@ class GenerateQR():
                 self.driver = webdriver.Firefox(
                     executable_path="bin/geckodriver", firefox_options=options)
 
-    def main(self):
-        logging.info("opening browser")
+   
         self.driver.set_page_load_timeout(930)
         try:
             self.driver.get(GenerateQR.URLs["url"])
@@ -61,82 +67,56 @@ class GenerateQR():
             return
 
     def parameters(self, numero):
-        time.sleep(2)  # Let the user actually see something!
-        self.newmethod555('/html/body/div[2]/div/div[1]/ul/li[8]/a')
-
+        self.driver.implicitly_wait(5)        
+        self.findbyxpath('/html/body/div[2]/div/div[1]/ul/li[8]/a') # RAW button
+        self.findbyxpath('//*[@id="s1"]') # definir tamanho e cor button
+                                  
         txtarea = self.driver.find_element_by_id('text')
         txtarea.clear()
-        txtarea.send_keys('parametro de geração')
-        # time.sleep(2)  # Let the user actually see something!
-
-        btn_tamanho_cor_xpath = '//*[@id="s1"]'
-        btn_tamanho_cor = self.driver.find_element_by_xpath(
-            btn_tamanho_cor_xpath)
-        self.driver.execute_script("arguments[0].click()", btn_tamanho_cor)
+        txtarea.send_keys(numero)
 
         tamanho_imagem = self.driver.find_element_by_id('qrsi')
         self.driver.execute_script(
             "arguments[0].setAttribute('value','200')", tamanho_imagem)
 
-        size_frontier_xpath = '//*[@id="borderr"]/label[2]'
-        size_frontier = self.driver.find_element_by_xpath(size_frontier_xpath)
-        self.driver.execute_script("arguments[0].click()", size_frontier)
-
-        error_cor_xpath = '//*[@id="errr"]/label[4]'
-        error_cor = self.driver.find_element_by_xpath(error_cor_xpath)
-        self.driver.execute_script("arguments[0].click()", error_cor)
-
-        radius_xpath = '//*[@id="radiusr"]/label[2]'
-        radius = self.driver.find_element_by_xpath(radius_xpath)
-        self.driver.execute_script("arguments[0].click()", radius)
-
-        # time.sleep(2)  # Let the user actually see something!
-
-        btn_mais_logotipo = self.driver.find_element_by_xpath('//*[@id="s2"]')
-        self.driver.execute_script("arguments[0].click()", btn_mais_logotipo)
-
-        photo_xpath = '//*[@id="labelmode"]/label[3]'
-        photo = self.driver.find_element_by_xpath(photo_xpath)
-        self.driver.execute_script("arguments[0].click()", photo)
-
+        self.findbyxpath('//*[@id="borderr"]/label[2]') # tamanho da fronteira
+        self.findbyxpath('//*[@id="errr"]/label[4]') # correcao de cor
+        self.findbyxpath('//*[@id="radiusr"]/label[2]') # tamanho do raio
+        self.findbyxpath('//*[@id="s2"]') # definir logotipo
+        self.findbyxpath('//*[@id="labelmode"]/label[3]') # definir foto
+        
         sel_photo = self.driver.find_element_by_id('image')
         sel_photo_path = 'C:/dev/generate-qrcode-selenium/img/tabelionato_xisto.jpeg'
         sel_photo.send_keys(sel_photo_path)
 
-        dimension_xpath = '//*[@id="fontsize"]/label[4]'
-        dimension = self.driver.find_element_by_xpath(dimension_xpath)
-        self.driver.execute_script("arguments[0].click()", dimension)
-
-        # time.sleep(2)  # Let the user actually see something!
-
-        # <input type="file" class="custom-file-input" id="image">
-        # <img id="img-buffer" src="../dummy.png" style="display: none;">
-        # time.sleep(5)  # Let the user actually see something!
-
-        # image_src = self.driver.find_element_by_id('img-buffer')
-        # image_src = "arguments[0].setAttribute('src','" + \
-        #     "images/teste2.png" + "')"
+        self.findbyxpath('//*[@id="fontsize"]/label[4]') # dimensoes
 
         imagem = str(numero.strip() + '.png')
-        print(imagem)
         param = "arguments[0].setAttribute('download','" + imagem + "')"
+        self.driver.implicitly_wait(5)        
+
         baixa_qrcode = self.driver.find_element_by_id('dlbtn')
         self.driver.execute_script(param, baixa_qrcode)
 
-        time.sleep(5)  # Let the user actually see something!
-
         baixa_qrcode.click()
 
+        self.datauri(baixa_qrcode, imagem)
+
+    def datauri(self, baixa_qrcode, imagem):
+        """ Salva imagem com URI Basecode64."""
+        self.driver.implicitly_wait(5) 
         url = baixa_qrcode.get_property('href')
         uri = DataURI(url)
         with open("c:/xisto/" + imagem, 'wb') as f:
             f.write(uri.data)
 
-    def newmethod555(self, param_xpath):
-        element = self.driver.find_element_by_xpath(param_xpath)
-        self.driver.execute_script("arguments[0].click()", element)
-
-        # time.sleep(5)  # Let the user actually see something!
+    def findbyxpath(self, param_xpath):
+        """ Encontra elemento html com xpath."""
+        self.driver.implicitly_wait(5)        
+        try:
+            element = WebDriverWait(self.driver, 1).until(EC.presence_of_element_located((By.XPATH, param_xpath)))
+        finally:
+            self.driver.execute_script("arguments[0].click()", element)
 
     def close(self):
         self.driver.quit()
--- core.py
+++ core.py
@@ -2,13 +2,17 @@
 # -*- coding: utf-8 -*-
 """ Main program execute generate_qr_code."""
 from app.generate_qr import GenerateQR
+from time import sleep, time
 
 
 if __name__ == "__main__":
+    print('Inicio Geração QRCODE')
+    start_time = time()
     qr = GenerateQR()
-    qr.main()
-    # with open('numeros.txt') as f:
-    #     for line in f:
-    #         qr.parameters(str(line).strip())
-    qr.parameters("111111111111")
+    with open('numeros.txt') as f:
+        for line in f:
+            qr.parameters(str(line).strip())
     qr.close()
+    end_time = time()
+    elapsed_time = end_time - start_time
+    print(f'Fim geração - Tempo: {elapsed_time} segundos')
