8f69ce340f91fdf2d928bfd2455a76cbe0e30751
ricaportela <ricaportela@gmail.com>
2018-12-12 16:11:39 -0300

    Updates

--- core.py
+++ core.py
@@ -2,12 +2,17 @@
 # -*- coding: utf-8 -*-
 """ Main program execute generate_qr_code."""
 from app.generate_qr import GenerateQR
+from time import sleep, time
 
 
 if __name__ == "__main__":
+    print('Inicio Geração QRCODE')
+    start_time = time()
     qr = GenerateQR()
     with open('numeros.txt') as f:
          for line in f:
              qr.parameters(str(line).strip())
-    #qr.parameters("111111111111")
+    end_time = time()
+    elapsed_time = end_time - start_time
+    print(f'Fim geração - Tempo: {elapsed_time} segundos')
     qr.close()
